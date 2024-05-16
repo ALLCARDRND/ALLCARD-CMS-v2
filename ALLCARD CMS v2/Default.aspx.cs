bc93ff6439b785ae024ff0c0b993aca24d73ff8c
DevMarl <umbaomarliardo@gmail.com>
2024-05-16 10:01:48 +0800

    first commit

---
+++ ALLCARD CMS v2/Default.aspx.cs
@@ -0,0 +1,18 @@
+﻿using ALLCARD_CMS_v2.acms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ALLCARD_CMS_v2
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response.Redirect("acms/Dashboard.aspx");
+        }
+    }
+}
